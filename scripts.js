042d0479ee80309d490670efba9106de10bf122b
Lanseey <ldcabe@student.hau.edu.ph>
2025-02-12 22:31:46 +0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('.main-header');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) { // Adjust the scroll threshold as needed
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+});
+
